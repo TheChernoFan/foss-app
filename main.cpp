8bc5dad71bb7183cd99c162554e5d922e6d9a9f6
TheChernoFan <codwzm08.ph@gmail.com>
2024-03-19 21:10:31 +0800

    Base Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+    Program: Foss-App
+    Description: i dont really know
+    Author: qkrdusth
+    Goal: Make a foss app and a website for docs
+    2nd Goal: Make an app that's only one file
+*/
+
+#include <iostream>
+#include <string>
+
+struct Civilization 
+{
+    std::string mCivName;
+    int mCivPower;
+    std::string mCivAge; // Ancient, Classical, Medieval, etc.
+};
+
+int main() 
+{
+    Civilization japan;
+    std::cin.get();
+    return 0;
+}
